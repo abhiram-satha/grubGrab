557c50e50084f71c593384297a20576eb0932422
Abhiram <abhiram.satha@gmail.com>
2022-02-03 00:15:34 +0000

    Pushing the key ingredients seed

---
+++ db/seeds/04_keyingredients.sql
@@ -0,0 +1,9 @@
+INSERT INTO keyingredients (main_ingredient, available) VALUES ('Meat', TRUE);
+INSERT INTO keyingredients (main_ingredient, available) VALUES ('Seafood', FALSE);
+INSERT INTO keyingredients (main_ingredient, available) VALUES ('Vegetarian', TRUE);
+INSERT INTO keyingredients (main_ingredient, available) VALUES ('Desserts', TRUE);
+INSERT INTO keyingredients (main_ingredient, available) VALUES ('Drinks', TRUE);
+
+
+
+
