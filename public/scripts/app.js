815e8a6abea92259182fc8f27c0c060a847b7724
Brian Duong <brianminhduong@gmail.com>
2022-02-04 06:42:52 +0000

    Connect to database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,12 @@
 // Client facing scripts here
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "labber",
+  password: "labber",
+  host: "localhost",
+  database: "midterm",
+});
+
+//Connect to database
+pool.connect;
--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -1,9 +1,16 @@
 $(document).ready(function () {
+  //Item counter
+  $("button").click(function () {
+    const $cart = $(".cart div");
+    console.log($cart);
+    let cartNumber = $cart[0]["innerText"];
+    cartNumber++;
+    $cart[0]["innerText"] = cartNumber;
+  });
   //Show/Hide Cart Menu
   $(".cart").click(function () {
     const $cartOrders = $(".orders");
     const displayValue = $cartOrders.css("display");
-
     if (displayValue === "none") {
       $cartOrders.slideDown();
     } else {
