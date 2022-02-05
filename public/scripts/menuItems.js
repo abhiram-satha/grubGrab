c2e9e1df2207c16ac847e8322fd8708eceba6f77
Abhiram <abhiram.satha@gmail.com>
2022-02-05 00:56:35 +0000

    Fixing the menuitems function

--- public/scripts/menuItems.js
+++ public/scripts/menuItems.js
@@ -1,24 +1,27 @@
-const { load } = require('sass');
-const db = require('/server');
+// const { load } = require('sass');
+// const db = require('/server');
 
 $(document).ready (function() {
 
   $('.meat').click(function(){
-      const createMeatMenu = function (meal) {
 
-      let $meatMenu = $("<article>").addClass('new-items')
+    // alert('hello');
+      const createMeatMenu = function () {
 
+        console.log('hi')
+      $("article").addClass('new-items')
+      //   console.log($meatMenu);
       let menuItems = `
       <section class="food-card-holder">
         <div class="food-card">
           <section class="food-info">
             <div>
-              <img class="image" src="${meal.image}" alt="${meal.name}">
+              <img class="image" src="https://www.cookingclassy.com/wp-content/uploads/2013/02/chicken-parmesan-16.jpg" alt="chicken">
             </div>
             <div class="desc">
-              ${meal.name}
-              ${meal.price}
-              ${meal.description}
+              Chicken Parmasen
+              $12.99
+              boneless chicken breast, eggs, parmesan cheese
             </div>
           </section>
           <button>
@@ -27,37 +30,38 @@ $(document).ready (function() {
         </div>
       </section>`;
 
-      let result = $meatMenu.append(menuItems);
+      let result = $('article').append(menuItems);
 
       return result;
     }
 
+    createMeatMenu();
 
-    const renderMeals = (meals) => {
-      const mealContainer = $('#meal-container').empty();
+    // const renderMeals = (meals) => {
+    //   const mealContainer = $('#meal-container').empty();
 
-      for (const mealItem of meals) {
-        const mealInfo = {
-          image: mealItem.image,
-          name: mealItem.name,
-          price: mealItem.price,
-          description: mealItem.description
-        };
+    //   for (const mealItem of meals) {
+    //     const mealInfo = {
+    //       image: mealItem.image,
+    //       name: mealItem.name,
+    //       price: mealItem.price,
+    //       description: mealItem.description
+    //     };
 
-        const mealElement = createMeatMenu(mealInfo);
-        mealContainer.append(mealElement);
-      }
+    //     const mealElement = createMeatMenu(mealInfo);
+    //     mealContainer.append(mealElement);
+    //   }
 
-    }
+    // }
 
-    const loadTweets = () => db
-      .query(`
-          SELECT * FROM menuitems`)
-      .then((result) => {
-        return renderMeals(result.rows);
-      });
+    // const loadTweets = () => db
+    //   .query(`
+    //       SELECT * FROM menuitems`)
+    //   .then((result) => {
+    //     return renderMeals(result.rows);
+    //   });
 
-    loadTweets();
+    // loadTweets();
 
 
 
